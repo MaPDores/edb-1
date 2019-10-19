1b9f06b89b2fc0ff416fb4337f3c8d32c7caf696
MaPDores <peralta728@gmail.com>
2019-10-19 16:19:45 -0300

    Connect finish and working properly

--- server/main.c
+++ server/main.c
@@ -1,6 +1,15 @@
 #include "../lib/connect/connect.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+response requestHandler(request req) {
+    response res;
+    printf("Server received -%d- as request body\n", req.data);
+    res.status = 1;
+    return res;
+}
 
 int main () {
-    char port[] = "35671";
-    initServer(port);
+    // char port[] = "35671";
+    exit(initServer("35671", &requestHandler));
 }
